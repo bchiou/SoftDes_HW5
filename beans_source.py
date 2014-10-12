fb6c1643d4455b0f233dcdf4b3068260302a9993
Brandon <brandon.chiou@students.olin.edu>
2014-10-12 19:21:27 -0400

    beans source code r1

---
+++ beans_source.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 12 19:20:36 2014
+
+@author: brandon
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 10 14:26:33 2014
+
+@author: brandon
+"""
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
