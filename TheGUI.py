8e981fd7f2cd65b427e08225590d432feadd8b1a
Elena <eshaw2@wellesley.edu>
2014-10-13 23:39:06 -0400

    added background Surface and caption

--- TheGUI.py
+++ TheGUI.py
@@ -5,10 +5,12 @@ Created on Sun Oct 12 19:49:18 2014
 @author: elena
 """
 
-import pygame, sys
+import pygame, sys, random
 from pygame.locals import *
 Clock = pygame.time.Clock()
-vine = pygame.image.load('vine.jpg')
+color = 0,160,255
+
+
 #screen = pygame.display.set_mode((560,560))
 
 class MainFrame:
@@ -18,6 +20,11 @@ class MainFrame:
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Jack and the Bean Stalk')
+        background = pygame.Surface(self.screen.get_size())
+        self.background = background.convert()
+
+        
 
     def MainLoop(self):        
         while True:
@@ -26,17 +33,28 @@ class MainFrame:
                     pygame.quit()                    
                     sys.exit()
 
+#class Vines:
+#    """creates vine maze"""
+def createvines():
+    vine = pygame.image.load('vine.jpg').convert()    
+    for i in range(6):
+        MainWindow.screen.blit(vine,(i*16+50,100))
 
+#class Update:
+#    MainWindow.screen.blit(MainWindow.background,(0,0)) 
+#    pygame.display.flip()
 
-while True:
-    MainWindow = MainFrame(560, 560)    
-    MainWindow.screen.blit(vine,(100,100))
+if __name__ == "__main__":
+    MainWindow = MainFrame(560, 560) 
+    MainWindow.background.fill(color)
+    MainWindow.screen.blit(MainWindow.background,(0,0))        
+    createvines()
     pygame.display.flip() 
     MainWindow.MainLoop()
-  
+    pygame.display.update()
+    Clock.tick(60)
     
 
-pygame.display.update()
-Clock.tick(60)
+
 
 
