b8f244f6a00d08d6036df1b730adb01cc571e542
Elena <eshaw2@wellesley.edu>
2014-10-19 15:46:30 -0400

    added arrow key recognition

--- TheGUI.py
+++ TheGUI.py
@@ -26,12 +26,7 @@ class MainFrame:
 
         
 
-    def MainLoop(self):        
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()                    
-                    sys.exit()
+
 
 class Vines:
     """creates vine maze"""
@@ -49,10 +44,10 @@ class Vines:
         # Build actual maze
         Z = numpy.zeros(shape, dtype=int)
         # Make aisles
-        for i in range(complexity):
+        for i in range(density):
             x, y = numpy.random.randint(0, shape[1] // 2) * 2, numpy.random.randint(0, shape[0] //2) * 2
             Z[y, x] = 1
-            for j in range(density):
+            for j in range(complexity):
                 neighbours = []
                 if x > 1:             
                     neighbours.append((y, x - 2))
@@ -69,44 +64,99 @@ class Vines:
                         Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1
                         x, y = x_, y_
         return Z
-    def update(self):
-        if self.state == 'idle':
-            print 'Idling'
+#    def update(self):
+#        if self.state == 'idle':
+#            print 'Idling'
     def draw(self,screen):
         vine = pygame.image.load('vine.jpg').convert() 
-        Z = self.maze(.75, .20)
+       # Z = self.maze(.75, .20)
+        Z=self.maze(1,1)
+        V = []
         for i in range(len(Z)):
-            for j in range(len(Z)):            
-                if Z[i,j] != 0:
-                    MainWindow.screen.blit(vine, (i,j*4))
+            for j in range(len(Z)):
+#                print Z[i]
+#                print Z[j]                
+#                print Z[i,j]
+                if Z[i][j] != 0: #if Z[i][j]
+                    V.append((i*2,j*2))
+                    MainWindow.screen.blit(vine, (i*2,j*2))
+        return V
     
-class char(pygame.sprite.Sprite):
-#    def __init__(self, jcolor = "red", x, y):,
-#        pygame.sprite.Sprite. 
+#class burn:
+#    def burn(self, speed):
+#        
+
+class char:
+    """ Encodes the state of the paddle in the game """
+    def __init__(self):
+        self.state = 'idle'
+        self.x = 200
+        self.y = 200
+   
+    def draw(self,screen):
+        self.jackpic = pygame.image.load('snake.png').convert()        
 #        self.color = jcolor
-#        self.size = jsize
+        self.size = 1
+
+        MainWindow.screen.blit(self.jackpic, (self.x, self.y))
         
-    def move(self, xnew, ynew):
-        self.x = xnew
-        self.y = ynew
+
+class PyGameKeyController:
+    def __init__(self,model):
+        self.model = model
+    
+    def handle_key_event(self,event):
+        if event == pygame.K_LEFT:
+            print 'LEFT!'
+            new_x = Jack.x - 5
+            print new_x
+            MainWindow.screen.blit(char.draw.jackpic,(new_x, Jack.y))
+        if event == pygame.K_RIGHT:
+            print 'right!'
+            self.new.x = char.draw.x + 5
+            print self.new.x
+            MainWindow.screen.blit(jack,(new_x, Jack.y))
+
 
 
 if __name__ == "__main__":
     Clock.tick(60)    
     MainWindow = MainFrame() 
+    Jack = char()
+    controller = PyGameKeyController(Jack)
+   
     MainWindow.background.fill(color)
-    maze = Vines()
-    maze.maze(.75, .20)
-    MainWindow.screen.blit(MainWindow.background,(0,0))        
-    maze.update()
-    maze.draw(MainWindow)
-    pygame.display.flip() 
-    MainWindow.MainLoop()
-    pygame.display.update()
+#    maze = Vines()
 
-    jack = char(x = 0, y = 0)
-    jack.move(5,10)
-    print char.move.x
+
+    
+#    maze.maze(.75, .20)
+    MainWindow.screen.blit(MainWindow.background,(0,0))
+    Jack.draw(MainWindow)
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                controller.handle_key_event(event.key)
+#        maze.update()
+#        maze.draw(MainWindow)
+        pygame.display.flip() 
+        pygame.display.update()
+#        Clock.sleep(.001) 
+        
+#    maze.update()
+#    maze.draw(MainWindow)
+#    pygame.display.flip() 
+#    MainWindow.MainLoop()
+#    pygame.display.update()
+
+#    jack = char(x = 0, y = 0)
+#    jack.move(5,10)
+#    print char.move.x
 
 
 
