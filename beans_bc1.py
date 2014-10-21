166d901ff3b2b721568bfc5d56f0d2925ed7546b
Brandon <brandon.chiou@students.olin.edu>
2014-10-20 22:30:11 -0400

    brandon_r10: MVC ACTUALLY WORKING THIS TIME

--- beans_bc1.py
+++ beans_bc1.py
@@ -4,7 +4,7 @@ Created on Sun Oct 12 19:49:18 2014
 """
 
 import numpy
-import pygame, sys, random
+import pygame, sys, random, time
 from pygame.locals import *
 Clock = pygame.time.Clock()
 color = pygame.Color(0,160,255)
@@ -27,14 +27,13 @@ class View:
         #This class (View class) will print self.text/the ACTUAL vines
 
     def viewprint(self):
-        print self.model.vines.text
+        #print self.model.vines.text
         self.screen.fill(color)
-        pygame.draw.rect(self.screen, pygame.Color(255,0,0), (self.model.jack.x-10, self.model.jack.y-10, 10, 10))
+        pygame.draw.rect(self.screen, pygame.Color(255,0,0), (self.model.jack.x, self.model.jack.y, 10, 10))
         pygame.display.update()
 #####################################################################        
 class Vines: 
-    #If this doesn't work, put outside of Model class
-    #Abstracting for now --- blank vines
+    #Abstracting for now --- INSERT VINE CODE HERE
     def __init__(self):
         self.text = '""fucking vines"-that can be interpreted two ways"-Pratool Gadtaula' 
         
@@ -48,35 +47,35 @@ class Model:
 class Controller:
     def __init__(self, model):
         self.model = model    
-    def getinput(self):
-        self.model.vines.text = raw_input('Fuck this shit\n')
-        
+    def handle_key_event(self, event):
+        if event.type == KEYUP:
+            if event.key == pygame.K_UP:
+                self.model.jack.y -= 10
+            if event.key == pygame.K_DOWN:
+                self.model.jack.y += 10
+            if event.key == pygame.K_LEFT:
+                self.model.jack.x -= 10
+            if event.key == pygame.K_RIGHT:
+                self.model.jack.x += 10
 #####################################################################
 class Jack:
     def __init__(self):
-        self.image = pygame.image.load('snake.png')
-        self.image.set_colorkey(self.image.get_at((0,0)))
-        self.rect = self.image.get_rect()
-        self.x = height
-        self.y = width
-        self.type = 'player'
-        self.facing = 'up'
-        self.speed = 5
-        self.lives = 3
-        self.invincible = False
-        self.dead = False
-        self.paused = False
+        self.x = height - 10
+        self.y = width - 10
+
 #####################################################################               
 if __name__ == "__main__":
     gameModel = Model()
     gameView = View(gameModel)
     gameCont = Controller(gameModel)
-    running=True    
-    gameCont.getinput()
+    running=True
     
     while running:
         for event in pygame.event.get():
-            if event.type == QUIT: running = False
+            if event.type == QUIT: 
+                running = False
+            else:
+                gameCont.handle_key_event(event)
         gameView.viewprint()
         time.sleep(0.001)
     pygame.quit()
