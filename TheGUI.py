a50fa88d04ef8349d17e0a6cb91b9fdf825e614a
Elena <eshaw2@wellesley.edu>
2014-10-26 17:57:59 -0400

    boundried and starts

--- TheGUI.py
+++ TheGUI.py
@@ -4,13 +4,13 @@ Created on Sun Oct 12 19:49:18 2014
 @author: Elena Shaw & Brandon Chiou
 """
 
-import numpy
 import pygame, sys, random
 from pygame.locals import *
+
 Clock = pygame.time.Clock()
 color = 0,160,255
-height = 560
-width = 560
+HEIGHT = 560
+WIDTH = 560
 
 #screen = pygame.display.set_mode((560,560))
 
@@ -18,141 +18,157 @@ class MainFrame:
     """Initializes and creates game"""
     def __init__(self):
         pygame.init()
-        self.width = width
-        self.height = height
+        self.width = WIDTH
+        self.height = HEIGHT
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('Jack and the Bean Stalk')
         background = pygame.Surface(self.screen.get_size())
         self.background = background.convert()
 
+        
+
+    def MainLoop(self):        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()                    
+                    sys.exit()
+
+class MakeMaze:
+    """calculate a maze path"""
+    def __init__(self):
+        self.state = 'up'
+        self.width = WIDTH
+        self.height = HEIGHT
+    
+    def turn_R(self, pos, step):
+        return (pos[0] + step, pos[1])
+
+    def turn_L(self, pos, step):
+        return (pos[0] - step, pos[1])
+        
+    def go_up(self, pos, step):
+        return (pos[0], pos[1] - step)
+    
+    def build_maze(self, start, end, steprange): 
+        self.directions = [self.turn_R, self.turn_L, self.turn_L, self.go_up]
+        currentpos = (start, self.height-4)
+        list_of_points = [currentpos]
+    
+        if currentpos[1] == 0 & currentpos[0] == 0:
+            return list_of_points
+        while currentpos[1] > end or currentpos[0] > end:
+            f = random.choice(self.directions)
+            currentpos = f(currentpos, random.randint(steprange[0], steprange[1]))
+            #Check if the vine is out of bounds
+            if currentpos[0] < 0:
+                currentpos = (0, currentpos[1])
+            if currentpos[0] > self.width:
+                currentpos = (self.width-4, currentpos[1])
+            if currentpos[1] < 0:
+                currentpos = (currentpos[0], 0)
+            #Add the point to the list of points the vine goes through
+            list_of_points.append(currentpos)
+        return list_of_points
+
+    def dead_ends(self):
+        rn_W = random.randint(int(self.width*.1), 560)
+        rn_H = random.randint(0, 560)
+        return self.build_maze(rn_W, rn_H, (8, 28))
+
+
 class Vines:
     """creates vine maze"""
-    def __init__(self):
-        self.state = 'idle'
-
-    def maze(self, complexity, density):
-        H = height
-        W = width        
-        # Only odd shapes
-        shape = ((H // 2) * 2 + 1, (W // 2) * 2 + 1)
-        # Adjust complexity and density relative to maze size
-        complexity = int(complexity * ((shape[0] + shape[1])//2))
-        density    = int(density * (shape[0] // 2 + shape[1] // 2))
-        # Build actual maze
-        Z = numpy.zeros(shape, dtype=int)
-        # Make aisles
-        for i in range(density):
-            x, y = numpy.random.randint(0, shape[1] // 2) * 2, numpy.random.randint(0, shape[0] //2) * 2
-            Z[y, x] = 1
-            for j in range(complexity):
-                neighbours = []
-                if x > 1:             
-                    neighbours.append((y, x - 2))
-                if x < shape[1] - 2:  
-                    neighbours.append((y, x + 2))
-                if y > 1:             
-                    neighbours.append((y - 2, x))
-                if y < shape[0] - 2:  
-                    neighbours.append((y + 2, x))
-                if len(neighbours):
-                    y_,x_ = neighbours[numpy.random.randint(0, len(neighbours) - 1)]
-                    if Z[y_, x_] == 0:
-                        Z[y_, x_] = 1
-                        Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1
-                        x, y = x_, y_
-        return Z
-#    def update(self):
-#        if self.state == 'idle':
-#            print 'Idling'
-    def draw(self,screen):
-        vine = pygame.image.load('vine.jpg').convert() 
-       # Z = self.maze(.75, .20)
-        Z=self.maze(1,1)
-        V = []
-        for i in range(len(Z)):
-            for j in range(len(Z)):
-#                print Z[i]
-#                print Z[j]                
-#                print Z[i,j]
-                if Z[i][j] != 0: #if Z[i][j]
-                    V.append((i*2,j*2))
-                    MainWindow.screen.blit(vine, (i*2,j*2))
-        return V
-
-class CharControl(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('snake.png')
-        self.image.set_colorkey(self.image.get_at((0,0)))
-#        self.model = model
-        self.rect = self.image.get_rect()
-        self.x = height
-        self.y = width
-        self.type = 'player'
-        self.facing = 'up'
-        self.speed = 5
-        self.lives = 3
-        self.invincible = False
-        self.dead = False
-        self.paused = False
-        
-    def draw(self,screen):
-        self.jackpic = pygame.image.load('snake.png').convert()
-        self.size = 1
-        MainWindow.screen.blit(self.jackpic, (self.x, self.y))
+    def __init__(self, vcolor = (0,150,100)):
+        self.color = vcolor
+        self.width = WIDTH
+        self.height = HEIGHT
     
-    def handle_key_event(self, event):
-        if event == pygame.K_LEFT:
-            print 'LEFT!'
-            if self.x in range (0, height):
-                new_x = self.x - 5 #something WRONG here
-                print new_x
-                MainWindow.screen.blit(self.jackpic,(new_x, Jack.y))
+    def increasepix(self, obj):
+        if obj < 0:
+            newobj = obj-4
+            return int(newobj)
+        if obj >= 0:
+            newobj = obj+4
+            return int(newobj)
+
+    def drawSolu(self):
+#        vine = pygame.image.load('vine.jpg').convert()                     
+        vinemaze = MakeMaze()
+        solulist = vinemaze.build_maze(self.width-4, 0, (8,24))
+        
+        #draw starting point square
+        start = pygame.Rect(self.width, self.height, -4, -4)
+        pygame.draw.rect(MainWindow.background, self.color, start)
+
+        
+        for i in range(1, len(solulist)):
+#        for i in range(1, 10):
+            cur_coord = solulist[i]
+            prev_coord = solulist[i-1]
+            RectW= prev_coord[0] - cur_coord[0]
+            RectH = prev_coord[1] - cur_coord[1]
+            if abs(RectW) < abs(RectH):
+                newRectW = self.increasepix(RectW)
+                vine = pygame.Rect(cur_coord[0], cur_coord[1], newRectW, RectH)
+                pygame.draw.rect(MainWindow.background, self.color, vine)
             else:
-                #doNOTHING
-        if event == pygame.K_RIGHT:
-            print 'right!'
-            new_y = self.y + 5
-            print self.new.x
-            MainWindow.screen.blit(self.jackpic,(new_x, Jack.y))
-#        if event == pygame.K_UP:
-#        
-#        if event == pygame.K_DOWN:
+                newRectH = self.increasepix(RectH)
+                vine = pygame.Rect(cur_coord[0], cur_coord[1], RectW, newRectH)
+                pygame.draw.rect(MainWindow.background, self.color, vine)
+        MainWindow.screen.blit(MainWindow.background,(0,0))
+        pygame.display.update()
+        
+    def drawDead(self):
+        vinemaze = MakeMaze()
+        for x in xrange(10):  
+            mazelist = vinemaze.dead_ends()
+            for j in range(1, len(mazelist)):
+                coord = mazelist[j]
+                precoord = mazelist[j-1]
+                RectW2 = precoord[0] - coord[0]
+                RectH2 = precoord[1] - coord[1]
+                
+                if abs(RectW2) < abs(RectH2):
+                    newRectW2 = self.increasepix(RectW2)
+                    vine2 = pygame.Rect(coord[0], coord[1], newRectW2, RectH2)
+                    pygame.draw.rect(MainWindow.background, self.color, vine2)
+                else:
+                    newRectH2 = self.increasepix(RectH2)
+                    vine2 = pygame.Rect(coord[0], coord[1], RectW2, newRectH2)
+                    pygame.draw.rect(MainWindow.background, self.color, vine2)
+            
+            MainWindow.screen.blit(MainWindow.background,(0,0))  
+                
+        pygame.display.update()
+
+
+    
+def main():
+    directions = [MakeMaze.turn_R, MakeMaze.turn_L, MakeMaze.go_up] 
+    maze = Vines()
+#    MainWindow.screen.blit(MainWindow.background,(0,0))        
+#    maze.update()
+    maze.drawSolu()
+#    maze.drawDead()
+    
+
+
 
 if __name__ == "__main__":
+
+    
     Clock.tick(60)    
     MainWindow = MainFrame() 
-#    Jack = char()
-    controller = CharControl()
-    controller.draw(MainWindow)
-    
     MainWindow.background.fill(color)
-#    maze = Vines()
-#    maze.maze(.75, .20)
-    MainWindow.screen.blit(MainWindow.background,(0,0))
+   
+    main()
+    
+    pygame.display.flip() 
+    MainWindow.MainLoop()
+    pygame.display.update()
 
-#    maze.update()
-#    maze.draw(MainWindow)
 
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                controller.handle_key_event(event.key)
 
-        pygame.display.flip() 
-        pygame.display.update()
-#        Clock.sleep(.001) 
-        
-#    maze.update()
-#    maze.draw(MainWindow)
-#    pygame.display.flip() 
-#    MainWindow.MainLoop()
-#    pygame.display.update()
 
-#    jack = char(x = 0, y = 0)
-#    jack.move(5,10)
-#    print char.move.x
