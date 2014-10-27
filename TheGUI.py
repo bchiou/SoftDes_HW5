74e3a3954e55edb5fcae9032c1dbc0fa38b7b7a9
Elena <eshaw2@wellesley.edu>
2014-10-27 14:37:00 -0400

    commented final maze code

--- TheGUI.py
+++ TheGUI.py
@@ -8,14 +8,15 @@ import pygame, sys, random
 from pygame.locals import *
 
 Clock = pygame.time.Clock()
-color = 0,160,255
+color = 0,160,255 #color of GUI background
 HEIGHT = 560
 WIDTH = 560
 
-#screen = pygame.display.set_mode((560,560))
 
 class MainFrame:
-    """Initializes and creates game"""
+    """Initializes and creates game window.
+        Makes sure window will close when the
+        Quit button is pressed."""
     def __init__(self):
         pygame.init()
         self.width = WIDTH
@@ -35,7 +36,8 @@ class MainFrame:
                     sys.exit()
 
 class MakeMaze:
-    """calculate a maze path"""
+    """calculates a maze path using the node points at which
+        the maze will change directions"""
     def __init__(self):
         self.state = 'up'
         self.width = WIDTH
@@ -51,15 +53,22 @@ class MakeMaze:
         return (pos[0], pos[1] - step)
     
     def build_maze(self, start, end, steprange): 
+        """Builds exactly one randomized path leading from the starting 
+        point (x coordinate) to the end point (y coordinate),
+        and outpits a list of nodes based on the stepincreases of
+        (min steprange, max steprange)"""
+    
         self.directions = [self.turn_R, self.turn_L, self.turn_L, self.go_up]
         currentpos = (start, self.height-4)
         list_of_points = [currentpos]
     
         if currentpos[1] == 0 and currentpos[0] == 0:
             return list_of_points
+        #Before the end point is reached, move in a random direction
         while currentpos[1] > end or currentpos[0] > end:
             f = random.choice(self.directions)
             currentpos = f(currentpos, random.randint(steprange[0], steprange[1]))
+            
             #Check if the vine is out of bounds
             if currentpos[0] < 0:
                 currentpos = (0, currentpos[1])
@@ -72,19 +81,21 @@ class MakeMaze:
         return list_of_points
 
     def dead_ends(self):
-        rn_W = random.randint(int(self.width*.1), 560)
+        """Creates random additional deadend vines that are not solutions"""
+        rn_W = random.randint(int(self.width*.1), 560) #start 10% up from bottom of screen
         rn_H = random.randint(0, 560)
         return self.build_maze(rn_W, rn_H, (8, 28))
 
 
 class Vines:
-    """creates vine maze"""
+    """Draws vine rectangles using inputted node points"""
     def __init__(self, vcolor = (0,150,100)):
         self.color = vcolor
         self.width = WIDTH
         self.height = HEIGHT
     
     def increasepix(self, obj):
+        """Increase thickness of the vine"""
         if obj < 0:
             newobj = obj-4
             return int(newobj)
@@ -93,23 +104,27 @@ class Vines:
             return int(newobj)
 
     def drawSolu(self):
-#        vine = pygame.image.load('vine.jpg').convert()                     
+        """Draw the correct solution path outputted above from 'build_maze'"""                    
         vinemaze = MakeMaze()
         solulist = vinemaze.build_maze(self.width-4, 0, (8,24))
         
-        #draw starting point square
+        #draw starting point square at bottom right corner
         start = pygame.Rect(self.width, self.height, -4, -4)
         pygame.draw.rect(MainWindow.background, self.color, start)
         dimenlist = []
         
+        #determine the rectangle's starting coordinate, width, and height
         for i in range(1, len(solulist)):
-#        for i in range(1, 20):
             cur_coord = solulist[i]
             prev_coord = solulist[i-1]
             RectW = prev_coord[0] - cur_coord[0]
             RectH = prev_coord[1] - cur_coord[1]
+
+            #keep track of coordinates where non-overlapping vines occur            
             if RectH > 0:
                 dimenlist.append(prev_coord)
+            
+            #Increase thickness of vines, depending on if they lie vertical or horizontal            
             if abs(RectW) < abs(RectH):
                 newRectW = self.increasepix(RectW)
                 vine = pygame.Rect(cur_coord[0], cur_coord[1], newRectW, RectH)              
@@ -118,7 +133,7 @@ class Vines:
                 vine = pygame.Rect(cur_coord[0], cur_coord[1], RectW, newRectH)
             pygame.draw.rect(MainWindow.background, self.color, vine)
         
-        #fill in missing corners due to non overlapping rectangles
+        #fill in the missing corners due to non-overlapping vines
         for i in range(1, len(dimenlist)):
             prevW = dimenlist[i][0]
             prevH = dimenlist[i][1]
@@ -131,15 +146,18 @@ class Vines:
         pygame.display.update()
         
     def drawDead(self):
+        """Draw vines to the list of deadend nodes assigned under 'dead_ends'"""
         vinemaze = MakeMaze()
         for x in xrange(10):  
             mazelist = vinemaze.dead_ends()
+            #determine the rectangle's starting coordinate, width, and height
             for j in range(1, len(mazelist)):
                 coord = mazelist[j]
                 precoord = mazelist[j-1]
                 RectW2 = precoord[0] - coord[0]
                 RectH2 = precoord[1] - coord[1]
                 
+                 #Increase thickness of vines, depending on if they lie vertical or horizontal 
                 if abs(RectW2) < abs(RectH2):
                     newRectW2 = self.increasepix(RectW2)
                     vine2 = pygame.Rect(coord[0], coord[1], newRectW2, RectH2)
@@ -156,10 +174,9 @@ class Vines:
 
     
 def main():
+    """generate the random maze and draw it to the GUI background"""
     directions = [MakeMaze.turn_R, MakeMaze.turn_L, MakeMaze.go_up] 
     maze = Vines()
-#    MainWindow.screen.blit(MainWindow.background,(0,0))        
-#    maze.update()
     maze.drawSolu()
     maze.drawDead()
     
@@ -168,13 +185,15 @@ def main():
 
 if __name__ == "__main__":
 
-    
+    #Initiate the GUI interface    
     Clock.tick(60)    
     MainWindow = MainFrame() 
     MainWindow.background.fill(color)
    
+    #draw the maze
     main()
     
+    #update the GUI screen
     pygame.display.flip() 
     MainWindow.MainLoop()
     pygame.display.update()
